19edce1fc6a236eb27d120244369ce73515230f8
amandinha951 <amandinha951@gmail.com>
2024-05-25 19:54:58 -0300

    comentario 2

---
+++ assets/js/alunos/alunos.js
@@ -0,0 +1,6 @@
+import { createSidebar } from "../../js/sidebar/sidebar.js";
+import { sidebarItems } from "../../functions/alunos/alunos.js";
+
+window.onload = () => {
+    createSidebar(sidebarItems);
+};
