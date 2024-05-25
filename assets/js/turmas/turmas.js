fcc7399cafd5c8e509b8d7b6adc7d8404d896580
LucasF4biano <Lucas12042001@gmail.com>
2024-05-25 14:35:31 -0300

    arrumando diretorio

---
+++ assets/js/turmas/turmas.js
@@ -0,0 +1,5 @@
+import { createSidebar } from "../../js/sidebar/sidebar.js";
+import{sidebarItems} from '../../functions/turmas/turmas.js'
+window.onload = () => {
+  createSidebar(sidebarItems);
+};
