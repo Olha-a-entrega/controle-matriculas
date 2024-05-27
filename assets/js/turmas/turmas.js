e85cf4eb2c325185f29591ee4276108a64b5b0eb
LucasF4biano <Lucas12042001@gmail.com>
2024-05-27 09:38:57 -0300

    removendo linha

--- assets/js/turmas/turmas.js
+++ assets/js/turmas/turmas.js
@@ -4,3 +4,19 @@ import { sidebarItems } from "../../functions/turmas/turmas.js";
 window.onload = () => {
   createSidebar(sidebarItems);
 };
+
+document.addEventListener('DOMContentLoaded', function() {
+  // Seleciona todos os botões de remover turma
+  var removeTurmaButtons = document.querySelectorAll('.bg-transparent.border-0[title="Remover turma"]');
+
+  // Adiciona um evento de clique a cada botão
+  removeTurmaButtons.forEach(function(button) {
+    button.addEventListener('click', function() {
+      // Obtém a linha da tabela que contém o botão clicado
+      var row = this.closest('tr');
+      
+      // Remove a linha da tabela
+      row.parentNode.removeChild(row);
+    });
+  });
+});
